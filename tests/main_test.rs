3128b754a2abe8a17b696474ce1b510f86bc3cd5
James Usevitch <james_usevitch@byu.edu>
2024-09-02 14:56:57 -0600

    Updated test file and added micro_algebra repo

--- tests/main_test.rs
+++ tests/main_test.rs
@@ -1,9 +1,10 @@
-use rhoflight::rosflight::ROSFlight;
+use rustflight_alpha::rustflight::ROSFlight;
+use rustflight_alpha::board::DummyBoard;
 
 #[test]
 fn main_test() {
 
-    let firmware = ROSFlight::init(0);
+    let firmware = ROSFlight::init(0, Box::new(DummyBoard{}));
 
     for _i in 0..10 {
         firmware.run();
