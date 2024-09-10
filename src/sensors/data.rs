a9a0e595411d31e20a7532c3328b87dca402e3d9
James Usevitch <james_usevitch@byu.edu>
2024-09-09 23:26:38 -0600

    Finished GNSSFull struct. Some revisions still needed.

--- src/sensors/data.rs
+++ src/sensors/data.rs
@@ -1,4 +1,9 @@
-use crate::units::{MM, CM, UnsignedMM, UnsignedCM, CMPerSec, UnsignedCMPerSec, DegENeg7, UnixTimeSeconds, FracTime, MMPerSec,ROSFlightTimestamp};
+use crate::units::{
+    MM, CM, UnsignedMM, UnsignedCM, CMPerSec, UnsignedCMPerSec, DegENeg7,
+    UnixTimeSeconds, FracTime, MMPerSec, ROSFlightTimestamp, TimeOfWeek,
+    Year, Month, Day, Hour, Minute, Second, Nanosecond, Longitude,
+    Latitude, Meter, MeterPerSec, Radians,
+};
 
 // Newtype pattern for units to prevent conversion errors.
 
@@ -33,36 +38,59 @@ struct GNSSData {
 }
 
 
+/*
+TODO: Fix type values taken from MAVLink; change if necessary
+https://mavlink.io/en/messages/common.html#GPS_FIX_TYPE
+ */
+#[derive(Default, Debug)]
+pub enum FixType {
+    #[default] // Sets default field to NoGPS
+    NoGPS,
+    NoFix,
+    Fix2D,
+    Fix3D,
+    DGPS,
+    RTKFloat,
+    RTKFixed,
+    Static,
+    PPP,
+}
+
+/*
+Many of these fields seem to have been partially taken or inspired by
+related MAVLink fields.
+
+TODO: Figure out units for these fields.
+ */
 #[derive(Default)]
 pub struct GNSSFull {
-    uint64_t time_of_week;
-    uint16_t year;
-    uint8_t month;
-    uint8_t day;
-    uint8_t hour;
-    uint8_t min;
-    uint8_t sec;
-    uint8_t valid;
-    uint32_t t_acc;
-    int32_t nano;
-    uint8_t fix_type;
-    uint8_t num_sat;
-    int32_t lon;
-    int32_t lat;
-    int32_t height;
-    int32_t height_msl;
-    uint32_t h_acc;
-    uint32_t v_acc;
-    int32_t vel_n;
-    int32_t vel_e;
-    int32_t vel_d;
-    int32_t g_speed;
-    int32_t head_mot;
-    uint32_t s_acc;
-    uint32_t head_acc;
-    uint16_t p_dop;
-    uint64_t rosflight_timestamp; // microseconds, time stamp of last byte in the message
-
+    time_of_week: TimeOfWeek,
+    year: Year,
+    month: Month,
+    day: Day,
+    hour: Hour,
+    min: Minute,
+    sec: Second,
+    valid: u8, // Units?
+    t_acc: i32, // Units?
+    nano: Nanosecond,
+    fix_type: FixType,
+    num_sat: u8,
+    lon: Longitude,
+    lat: Latitude,
+    height: Meter, // Units? MAVLink specifies meters
+    height_msl: Meter, // Units?
+    h_acc: Meter, // Units?
+    v_acc: MeterPerSec, // Units?
+    vel_n: MeterPerSec,
+    vel_e: MeterPerSec,
+    vel_d: MeterPerSec,
+    g_speed: MeterPerSec, // Units? This seems to be ground speed
+    head_mot: i32, // Units? Not sure what this field represents
+    s_acc: Second, // Units? Seems to be seconds accuracy...?
+    head_acc: Radians, // Units?
+    p_dop: u16, // Units? Not sure what this represents
+    rosflight_timestamp: ROSFlightTimestamp, // microseconds, time stamp of last byte in the message
 }
 
 
