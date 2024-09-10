972adeb4b5ab6f20bb81257e6653000d96c9a3ef
James Usevitch <james_usevitch@byu.edu>
2024-09-09 23:27:14 -0600

    Changed ROSFlight struct to be generic over the board satisfying Board trait

--- src/rustflight.rs
+++ src/rustflight.rs
@@ -1,17 +1,17 @@
 
 use crate::board::Board;
 
-pub struct ROSFlight {
+pub struct ROSFlight<B: Board> {
     loop_time_us: u32,
     /* TODO: Is `Box<>` the best way to do this? Ensures that we use the Board trait, but requires
     heap allocation */
-    board: Box<dyn Board>,
+    board: B,
 }
 
-impl ROSFlight {
+impl<B: Board> ROSFlight<B> {
     pub fn init(
         _loop_time_us: u32,
-        _board: Box<dyn Board>,
+        _board: B,
     ) -> Self {
         Self {
             loop_time_us: _loop_time_us,
