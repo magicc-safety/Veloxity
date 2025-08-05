fa14e9fde247d7fa7f2764a483189f09aed1a8ef
bascom16 <bascomb1@byu.edu>
2025-08-05 15:19:44 -0600

    Updated VCP to match telem functionality. Can work in its place by using VCP pipes in place

--- stm_32/src/peripherals/vcp.rs
+++ stm_32/src/peripherals/vcp.rs
@@ -4,22 +4,42 @@ use embassy_stm32::usb::{Driver, Instance};
 use embassy_stm32::peripherals::USB_OTG_FS;
 use defmt::{panic, *};
 use embassy_futures::join::join;
+use embassy_time::Timer;
 use embassy_usb::class::cdc_acm::{CdcAcmClass, State};
 use embassy_usb::driver::EndpointError;
 use embassy_usb::Builder;
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+use embassy_sync::pipe::Pipe;
 
-use rustflight_core::errors::SensorError;
+use rustflight_core::comm_manager::comm_link_trait::EmbeddedComInterface;
+use rustflight_core::errors::{self, SensorError};
 
-use crate::peripherals::sbus::RC_SIGNAL;
+pub const VCP_TX_BUFF_SIZE: usize = 2048;
+pub const VCP_RX_BUFF_SIZE: usize = 2048;
 
-pub struct Vcp {
-    pub driver: Driver<'static, USB_OTG_FS>,
+pub static VCP_TX: Pipe<CriticalSectionRawMutex, VCP_TX_BUFF_SIZE> = Pipe::new();
+pub static VCP_RX: Pipe<CriticalSectionRawMutex, VCP_RX_BUFF_SIZE> = Pipe::new();
+
+pub struct BasicProcessor;
+
+impl EmbeddedComInterface for BasicProcessor {
+    async fn process_bytes(&mut self, buf: &[u8], num_bytes: usize) {
+        VCP_RX.write_all(&buf[0..num_bytes]).await;
+    }
 }
 
-impl Vcp {
+pub struct Vcp<ECI: EmbeddedComInterface> {
+    pub driver: Driver<'static, USB_OTG_FS>,
+    pub byte_processor: ECI,
+}
 
-    pub async fn run(self){
+impl<ECI: EmbeddedComInterface> Vcp<ECI> {
+
+    pub async fn run(mut self){
         // Adapted from Embassy STM32H7 examples
+
+        let driver = self.driver;
+        let mut byte_processor = self.byte_processor;
 
         // Create embassy-usb Config
         let mut config = embassy_usb::Config::new(0xc0de, 0xcafe);
@@ -36,7 +56,7 @@ impl Vcp {
         let mut state = State::new();
 
         let mut builder = Builder::new(
-            self.driver,
+            driver,
             config,
             &mut config_descriptor,
             &mut bos_descriptor,
@@ -46,61 +66,69 @@ impl Vcp {
 
         // Create classes on the builder.
         let mut class = CdcAcmClass::new(&mut builder, &mut state, 64);
-
         // Build the builder.
         let mut usb = builder.build();
-
         // Run the USB device.
         let usb_fut = usb.run();
 
-        // Do stuff with the class!
-        let echo_fut = async {
+        let vcp_fut = async {
             loop {
                 class.wait_connection().await;
                 info!("Connected");
-                let result = echo(&mut class).await;
-                if let Err(e) = result {
-                    warn!("VCP echo error");
-                    // TODO: Signal error?
+                let result = Self::tx_rx(&mut byte_processor, &mut class).await;
+                if let Err(_) = result {
+                    warn!("VCP error");
                 }
-                // TODO: Currently an echo. What is it's purpose?
             }
         };
 
-        // Run everything concurrently.
-        // If we had made everything `'static` above instead, we could do this using separate tasks instead.
-        join(usb_fut, echo_fut).await;
+        join(usb_fut, vcp_fut).await;
+    }
+
+    async fn tx_rx<'d, T: Instance + 'd>(
+        byte_processor: &mut ECI, 
+        class: &mut CdcAcmClass<'d, Driver<'d, T>>,
+    ) -> Result<(), SensorError> {
+        const MAX_PACKET_SIZE: usize = 64;
+        let mut tx_buf = [0u8; VCP_TX_BUFF_SIZE];
+        let mut rx_buf = [0u8; VCP_RX_BUFF_SIZE];
+        loop {
+            // tx
+            // defmt::debug!("Waiting for data to send...");
+            let n = VCP_TX.read(&mut tx_buf).await;
+            // defmt::debug!("Read {} bytes from VCP_TX", n);
+            if n > 0 {
+                let data_to_send = &tx_buf[..n];
+                for chunk in data_to_send.chunks(MAX_PACKET_SIZE) {
+                    // defmt::debug!("Sending {} bytes to USB host", chunk.len());
+                    let result = class
+                        .write_packet(chunk)
+                        .await
+                        .map_err(|e| match e {
+                            _ => errors::TelemError::GenericTelemError("UsbTx failed!"),
+                        });
+                }
+            }
+            // rx
+            // defmt::debug!("Waiting for data from USB host...");
+            let result = class.read_packet(&mut rx_buf).await;
+            match result {
+                Err(_) => {
+                    // defmt::warn!("System: Usb read error");
+                    Timer::after_millis(1).await;
+                },
+                Ok(n) => {
+                    // defmt::debug!("Received {} bytes from USB host", n);
+                    if n > 0 && n <= VCP_RX_BUFF_SIZE {
+                        byte_processor.process_bytes(&rx_buf[..n], n).await;
+                    }
+                }
+            }
+        }
     }
 }
 
 #[embassy_executor::task]
-pub async fn task(mut vcp: Vcp ) {
+pub async fn task(mut vcp: Vcp<BasicProcessor>) {
     vcp.run().await;
-}
-
-async fn echo<'d, T: Instance + 'd>(class: &mut CdcAcmClass<'d, Driver<'d, T>>) -> Result<(), SensorError> {
-    let mut buf = [0; 64];
-    loop {
-        let n = match class.read_packet(&mut buf).await {
-            Ok(n) => n, 
-            Err(e) => { 
-                match e {
-                    EndpointError::BufferOverflow => {
-                        warn!("USB read buffer overflow!");
-                        Err(SensorError::GenericSensorError("USB buffer overflow"))
-                    } 
-                    EndpointError::Disabled => {
-                        info!("USB endpoint disabled.");
-                        Err(SensorError::GenericSensorError("USB endpoint disabled"))
-                    }
-                }
-            }?
-        };
-
-        let data = &buf[..n];
-        info!("data: {:x}", data);
-        class.write_packet(data).await.map_err(|e| match e {
-            _ => SensorError::GenericSensorError("USB failed: write packet"),
-        })?;
-    }
 }
