7d0d0784de41ce476beb56371ecf903357f2a22c
Tyler Larsen <trlarsen713@gmail.com>
2026-07-21 22:19:16 -0600

    Add --release flag.

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -72,7 +72,7 @@ fn main() -> ExitCode {
                 "veloxity",
             ])
         }
-        "build-sim-lib" => cargo(["build", "-p", "sim", "--lib"]),
+        "build-sim-lib" => cargo(["build", "-p", "sim", "--lib", "--release"]),
         "clean-generated" => clean_generated(),
         _ => {
             eprintln!("unknown command `{command}`");
